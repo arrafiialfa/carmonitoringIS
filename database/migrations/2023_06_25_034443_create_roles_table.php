b7ce9f3f9d09d569fc087b6a65e76d902c1905e9
arrafii <arrafiialfa.m@gmail.com>
2023-06-25 11:05:45 +0700

    add role seeder

--- database/migrations/2023_06_25_034443_create_roles_table.php
+++ database/migrations/2023_06_25_034443_create_roles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('management_level');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Carbon\Carbon;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Define the roles data
+        $currentTimestamp = Carbon::now();
+        $roles = [
+            [
+                'id' => '1',
+                'name' => 'Admin',
+                'description' => 'Responsible for CRUD operations',
+                'management_level' => 0,
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp,
+            ],
+            [
+                'id' => '2',
+                'name' => 'First-Line Manager',
+                'management_level' => 1,
+                'description' => null,
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp,
+            ],
+            [
+                'id' => '3',
+                'name' => 'Middle-Line Manager',
+                'management_level' => 2,
+                'description' => null,
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp,
+            ],
+            [
+                'id' => '4',
+                'name' => 'Top-Level Manager',
+                'management_level' => 3,
+                'description' => null,
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp,
+            ],
+        ];
+
+        // Insert the roles into the database
+        Role::insert($roles);
+    }
+}
